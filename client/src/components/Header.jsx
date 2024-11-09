779794c94438f4dc0500fa0cec0b5ce849039682
Boombm <nutruja.bm@gmail.com>
2024-11-10 05:24:14 +0700

    db connected + made express backend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 
 function Header() {
+  const [activeLink, setActiveLink] = useState("/");
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
     <header className="bg-white shadow-md py-2">
       <div className="flex justify-between max-w-7xl mx-auto items-center p-3">
-        <Link>
+        <Link to="/" onClick={() => handleLinkClick("/")}>
           <h1 className="font-bold text-lg sm:text-2xl flex items-center space-x-2">
             <img
               src="/logo.png"
@@ -14,48 +20,86 @@ function Header() {
               className="h-12 sm:h-14"
             />
             <div className="flex flex-col text-left">
-              <span className="text-primary-600 text-sm sm:text-xl leading-relaxed tracking-wide">
+              <span className="hidden md:inline text-primary-600 text-md sm:text-xl leading-relaxed tracking-wide">
                 BSN
               </span>
-              <span className="text-slate-700 text-xs sm:text-lg leading-relaxed tracking-wide">
+              <span className="hidden md:inline text-slate-700 text-sm sm:text-base leading-relaxed tracking-wide">
                 Real Estate
               </span>
             </div>
           </h1>
         </Link>
-        <ul className="flex gap-4">
-          <Link to="/">
-            <li className="hidden sm:inline py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500">
+
+        <ul className="flex gap-4 md:gap-8">
+          <Link to="/" onClick={() => handleLinkClick("/")}>
+            <li
+              className={`hidden md:inline py-2 px-3 rounded w-full text-center ${
+                activeLink === "/"
+                  ? "text-blue-700"
+                  : "text-gray-900 hover:text-blue-700"
+              }`}
+            >
               หน้าหลัก
             </li>
           </Link>
-          <Link to="/properties">
-            <li className="hidden sm:inline py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+
+          <Link to="/properties" onClick={() => handleLinkClick("/properties")}>
+            <li
+              className={`hidden md:inline py-2 px-3 rounded w-full text-center ${
+                activeLink === "/properties"
+                  ? "text-blue-700"
+                  : "text-gray-900 hover:text-blue-700"
+              }`}
+            >
               รายการอสังหาริมทรัพย์
             </li>
           </Link>
-          <Link to="/contact">
-            <li className="hidden sm:inline py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+
+          <Link to="/contact" onClick={() => handleLinkClick("/contact")}>
+            <li
+              className={`hidden md:inline py-2 px-3 rounded w-full text-center ${
+                activeLink === "/contact"
+                  ? "text-blue-700"
+                  : "text-gray-900 hover:text-blue-700"
+              }`}
+            >
               ติดต่อเรา
             </li>
           </Link>
         </ul>
+
         <form className="flex items-center p-3 rounded-lg border border-gray-300">
+          <FaSearch className="text-slate-600 mr-2" />
           <input
             type="text"
             placeholder="ค้นหา..."
             className="bg-transparent focus:outline-none w-24 sm:w-64 sm:px-2 text-base"
           />
-          <FaSearch className="text-slate-600 mr-2" />
         </form>
-        <ul className="flex gap-4">
-          <Link to="/register">
-            <li className="hidden sm:inline text-slate-700 hover:underline py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+
+        <ul className="flex gap-4 md:gap-8">
+          {/* <Link to="/register" onClick={() => handleLinkClick("/register")}>
+            <li
+              className={`hidden md:inline py-2 px-3 rounded w-full text-center ${
+                activeLink === "/register"
+                  ? "text-blue-700"
+                  : "text-gray-900 hover:text-blue-700"
+              }`}
+            >
               สมัครสมาชิก
             </li>
-          </Link>
-          <Link to="/login">
-            <li className="text-slate-700 hover:underline py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">เข้าสู่ระบบ</li>
+          </Link> */}
+
+          <Link to="/login" onClick={() => handleLinkClick("/login")}>
+            <li
+              className={`py-2 px-3 rounded w-full text-center ${
+                activeLink === "/login"
+                  ? "text-blue-700"
+                  : "text-gray-900 hover:text-blue-700"
+              }`}
+            >
+              เข้าสู่ระบบ
+            </li>
           </Link>
         </ul>
       </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Register() {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+    console.log(formData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-10">สมัครสมาชิก</h1>
-
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-6">
           <label
-            for="username"
+            htmlFor="username"
             className="block mb-2 text-base font-medium text-gray-900 dark:text-white"
           >
             ชื่อผู้ใช้
@@ -19,11 +38,12 @@ function Register() {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="กรุณากรอกชื่อผู้ใช้"
             required
+            onChange={handleChange}
           />
         </div>
         <div className="mb-6">
           <label
-            for="email"
+            htmlFor="email"
             className="block mb-2 text-base font-medium text-gray-900 dark:text-white"
           >
             อีเมล
@@ -34,11 +54,12 @@ function Register() {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="example@gmail.com"
             required
+            onChange={handleChange}
           />
         </div>
         <div className="mb-6">
           <label
-            for="password"
+            htmlFor="password"
             className="block mb-2 text-base font-medium text-gray-900 dark:text-white"
           >
             รหัสผ่าน
@@ -49,11 +70,12 @@ function Register() {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="กรุณากรอกรหัสผ่านที่มีความยาวอย่างน้อย 8 ตัวอักษร"
             required
+            onChange={handleChange}
           />
         </div>
         <div className="mb-6">
           <label
-            for="confirm_password"
+            htmlFor="confirm_password"
             className="block mb-2 text-base font-medium text-gray-900 dark:text-white"
           >
             ยืนยันรหัสผ่าน
@@ -64,20 +86,21 @@ function Register() {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="ยืนยันรหัสผ่านให้ตรงกับรหัสผ่านที่กรอกไว้ก่อนหน้านี้"
             required
+            // onChange={handleChange}
           />
         </div>
-        <div class="flex items-start mb-5">
-          <div class="flex items-center h-5">
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
             <input
               id="terms"
               type="checkbox"
               value=""
-              class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
               required
             />
           </div>
           <label
-            for="terms"
+            htmlFor="terms"
             className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
           >
             ยอมรับเงื่อนไขข้อตกลง{" "}
@@ -93,9 +116,9 @@ function Register() {
 
         <button
           type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="w-full py-3 px-5 text-lg text-white bg-[#5ece4f] rounded-lg mt-2 cursor-pointer font-medium"
         >
-          Submit
+          สมัครสมาชิก
         </button>
       </form>
       <div className="mt-4">
